6c77fa8105aeb8b9776e2aaa0f3692a074f21451
romadvincula <advi0001@flinders.edu.au>
2020-02-04 11:41:50 +1030

    removed cs50/homepage/homepage/

--- cs50/homepage/homepage/locate.js
+++
@@ -1,5 +0,0 @@
-function find(element) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        document.querySelector("#location").value = position.coords.latitude + ', ' + position.coords.longitude;
-    });
-}
